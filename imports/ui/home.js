fff43759bf60874591ace2dc407f13f3eafedfc6
Daniel Park <dpark@Daniel-Park.local>
2016-05-27 15:54:57 -0400

    init files

---
+++ imports/ui/home.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+// import { ReactiveDict } from 'meteor/reactive-dict';
+
+import './navigation.html';
+import './home.html';
+
+
+
